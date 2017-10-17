8aab7ac9beb865106b8af3f11d0462e96591e928
Ksen_008 <280952666@qq.com>
2017-10-17 10:03:50 +0800

    'add_aiding_method'

--- lists/tests.py
+++ lists/tests.py
@@ -13,13 +13,24 @@ class HomePageTest(TestCase):
     def test_home_page_return_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
+
         expected_html = render_to_string('home.html')
+        # print('a',expected_html)
+        # print('b',response.content.decode())
         self.assertEqual(response.content.decode(), expected_html)
+
 
     def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
         request.method = 'POST'
         request.POST['item_text'] = 'A new list item'
-
         response = home_page(request)
-        self.assertEqual('id_new_item', response.content.decode())
+
+        self.assertIn('A new list item',response.content.decode())
+        expected_html = render_to_string(
+        'home.html',
+        {'new_item_text': 'A new list item'}
+        )
+        print('a',response.content.decode())
+        print('b',expected_html)
+        self.assertEqual(response.content.decode(), expected_html)
--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
-# from django.http.response import HttpResponse
+from django.http.response import HttpResponse
 def home_page(request):
-    return render(request,'home.html')
+    # if request.method == 'POST':
+    #     return HttpResponse(request.POST['item_text'])
+    return render(request,'home.html',{
+        'new_item_text':request.POST.get('item_text',''),
+    })
