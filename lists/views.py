b46900ad7652d3dc50ead6ae2012d0441c79d2ea
Ksen_008 <280952666@qq.com>
2017-10-30 13:19:33 +0800

    Refactor hard-coded URLs out of templates

--- lists/views.py
+++ lists/views.py
@@ -4,20 +4,6 @@ from django.core.exceptions import ValidationError
 def home_page(request):
     return render(request,'home.html',)
 
-# def view_list(request,list_id):
-#     list_ = List.objects.get(id=list_id)
-#     error = None
-#     if request.method == "POST":
-#         try:
-#             item = Item.objects.create(text=request.POST['item_text'], list=list_)
-#             item.full_clean()
-#             item.save()
-#             return redirect('/lists/%d/' % (list_.id,))
-#         except ValidationError:
-#             error = "You can't have an empty list item"
-#     return render(request,'list.html',{'list': list_,'error': error})
-
-
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
     error = None
